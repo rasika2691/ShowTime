9f48114368a778fa158543e107de96dbd2b7f617
rasika2691 <Rasika2691@gmail.com>
2017-01-21 17:56:21 -0500

    feat(api): add sort and search service

--- server/ShowTime/src/main/java/io/rasika/ShowTime/service/MovieService.java
+++ server/ShowTime/src/main/java/io/rasika/ShowTime/service/MovieService.java
@@ -9,8 +9,30 @@ import io.rasika.ShowTime.entity.Movie;
 public interface MovieService {
 
 	public List<Movie> findAll();
+	
 	public Movie findOne(String mId);
+	
 	public Movie create(Movie mv);
+	
 	public Movie update(String mId,  Movie mv);
+	
 	public void delete(String mId);
+	
+    public List<Movie> findByType(String type);
+	
+	public List<Movie> findByYear(int year);
+	
+	public List<Movie> findByGenre(String genre);
+	
+	public Movie findByTitle(String title);
+
+	public List<Movie> sortByImdbRatings();
+	
+	public List<Movie> sortByYear();
+	
+	public List<Movie> sortByImdbVotes();
+	
+	public List<Movie> topRatedMovies();
+	
+	public List<Movie> topRatedSeries();
 }
