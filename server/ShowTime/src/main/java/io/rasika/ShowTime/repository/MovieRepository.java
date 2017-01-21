aad1f7f8a5d5aa2f1eb14e9d32ca62920c56bb68
rasika2691 <Rasika2691@gmail.com>
2017-01-21 17:54:20 -0500

    refractor(api):add sort and search method defination

--- server/ShowTime/src/main/java/io/rasika/ShowTime/repository/MovieRepository.java
+++ server/ShowTime/src/main/java/io/rasika/ShowTime/repository/MovieRepository.java
@@ -9,9 +9,30 @@ import io.rasika.ShowTime.entity.Movie;
 public interface MovieRepository {
 
 	public List<Movie> findAll();
+	
 	public Movie findOne(String mId);
+	
 	public Movie findByTitle(String title);
+	
 	public Movie create(Movie mv);
+	
 	public Movie update(Movie mv);
+	
 	public void delete(Movie mv);
+	
+    public List<Movie> findByType(String type);
+	
+	public List<Movie> findByYear(int year);
+	
+	public List<Movie> findByGenre(String genre);
+	
+	public List<Movie> sortByImdbRatings();
+	
+	public List<Movie> sortByYear();
+	
+	public List<Movie> sortByImdbVotes();
+	
+	public List<Movie> topRatedMovies();
+	
+	public List<Movie> topRatedSeries();
 }
