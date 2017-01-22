ea73b5073dc65647d1fb7c0b899e14a8fa3e0800
rasika2691 <Rasika2691@gmail.com>
2017-01-22 15:02:04 -0500

    feat(api):add mappings of comments to movie and commnets to user

--- server/ShowTime/src/main/java/io/rasika/ShowTime/entity/Comment.java
+++ server/ShowTime/src/main/java/io/rasika/ShowTime/entity/Comment.java
@@ -4,15 +4,28 @@ import java.util.UUID;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table
+@NamedQueries({
+	@NamedQuery(name="Comment.findAll", query="SELECT cmt from Comment cmt"),
+	@NamedQuery(name="Comment.findAllUserCommentsByMovieId", query="SELECT cmt from Comment cmt")
+})
 public class Comment {
    
 	@Id
 	private String CommentId;
 	private String Comment;
+	
+	@ManyToOne
+	private Movie mv;  //Many Comments to one movie
+	
+	@ManyToOne
+	private User ur;  // Many Comments to one user
 	
 	public Comment(){
 		this.CommentId = UUID.randomUUID().toString();
@@ -21,6 +34,22 @@ public class Comment {
 	public String getCommentId() {
 		return CommentId;
 	}
+	public Movie getMv() {
+		return mv;
+	}
+
+	public void setMv(Movie mv) {
+		this.mv = mv;
+	}
+
+	public User getUr() {
+		return ur;
+	}
+
+	public void setUr(User ur) {
+		this.ur = ur;
+	}
+
 	public void setCommentId(String commentId) {
 		CommentId = commentId;
 	}
