e7328b5c073fef568cc7275919fa85ee5fc5a8ba
rasika2691 <Rasika2691@gmail.com>
2017-01-21 19:06:55 -0500

    fix(api): method signature for user update and delete

--- server/ShowTime/src/main/java/io/rasika/ShowTime/repository/UserRepository.java
+++ server/ShowTime/src/main/java/io/rasika/ShowTime/repository/UserRepository.java
@@ -11,6 +11,6 @@ public interface UserRepository {
 	public User findOne(String uId);
 	public User findByEmail(String email);
 	public User create(User ur);
-	public User update(String uId,  User ur);
-	public void delete(String uId);
+	public User update( User ur);
+	public void delete(User ur);
 }
