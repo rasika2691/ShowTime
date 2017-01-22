85b560f822e7816af8cc3ea711deac5bb092b363
rasika2691 <Rasika2691@gmail.com>
2017-01-21 19:10:34 -0500

    feat(api): add user implementation logic

--- server/ShowTime/src/main/java/io/rasika/ShowTime/repository/UserRepositoryImpl.java
+++ server/ShowTime/src/main/java/io/rasika/ShowTime/repository/UserRepositoryImpl.java
@@ -2,47 +2,58 @@ package io.rasika.ShowTime.repository;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import org.springframework.stereotype.Repository;
 
 import io.rasika.ShowTime.entity.User;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
+	@PersistenceContext
+	private EntityManager em1;
 	@Override
 	public List<User> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+   	TypedQuery<User> query=em1.createNamedQuery("User.findAll",User.class);
+   		return query.getResultList();	
 	}
 
 	@Override
 	public User findOne(String uId) {
-		// TODO Auto-generated method stub
-		return null;
+		return em1.find(User.class,uId);
 	}
 	
 	@Override
 	public User findByEmail(String email) {
-		// TODO Auto-generated method stub
+		TypedQuery<User> query=em1.createNamedQuery("User.findByEmail",User.class);
+		query.setParameter("pEmail", email);
+		List<User> users=query.getResultList();
+		if(users!=null && users.size()==1){
+			return users.get(0);
+		}
 		return null;
 	}
 
 	@Override
 	public User create(User ur) {
-		// TODO Auto-generated method stub
-		return null;
+		 em1.persist(ur);
+		 return ur;
+		
 	}
 
 	@Override
-	public User update(String uId, User ur) {
-		// TODO Auto-generated method stub
-		return null;
+	public User update(User ur) {
+		return em1.merge(ur);
 	}
 
 	@Override
-	public void delete(String uId) {
-		// TODO Auto-generated method stub
-
+	public void delete(User ur) {
+		em1.remove(ur);
 	}
+
+	
 
 	
 
